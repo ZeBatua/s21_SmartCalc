ef2c0da491e20575db076dc5f0561336ad9172e4
Thalassa Plack <thalassa@mi-h6.msk.21-school.ru>
2022-12-19 16:10:19 +0300

    style

--- src/Smart_Calc_QT/calculator/calc_string.c
+++ src/Smart_Calc_QT/calculator/calc_string.c
@@ -1,17 +1,17 @@
 #include "../calc.h"
 
 double calc_string(char *start_string, char *x_string) {
-    char buf[256] = {'\0'};
-    strncpy(buf, start_string, 256);
-    int status = 0;
-    double result = 0.0, x_result = 0.0;
-    x_result = read_string(x_string, &status);
-    add_x_value_in_string(start_string, x_result);
-    result = read_string(start_string, &status);
-    
-    char nothing = '\0';
-    memset(start_string, nothing, 256);
-    strncpy(start_string, buf, 256);
+  char buf[256] = {'\0'};
+  strncpy(buf, start_string, 256);
+  int status = 0;
+  double result = 0.0, x_result = 0.0;
+  x_result = read_string(x_string, &status);
+  add_x_value_in_string(start_string, x_result);
+  result = read_string(start_string, &status);
+
+  char nothing = '\0';
+  memset(start_string, nothing, 256);
+  strncpy(start_string, buf, 256);
 
   return result;
 }
